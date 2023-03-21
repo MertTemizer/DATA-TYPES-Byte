17aca29839e36e3acdd827aab4b983c0f03d2ef4
Mert Temizer <mert10temizer@gmail.com>
2023-03-21 21:10:02 +0300

    Byte Data Type
    byte data type examination

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//DATA TPYES: byte -> 0-255 
+
+byte first = 11;
+byte second = 28;
+byte third = 255;
+
+
+Console.WriteLine(first);
+Console.WriteLine(second);
+Console.WriteLine(third);
+
+byte maxDeger = byte.MaxValue;
+byte minDeger = byte.MinValue;
+
+Console.WriteLine(maxDeger);
+Console.WriteLine(minDeger);
+
